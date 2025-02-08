82cbb71764499f2c1cee1b31d664c040405cb432
Reuben B <112469788+reubs01@users.noreply.github.com>
2025-02-08 12:02:00 +1100

    Add files via upload

---
+++ checkBitlocker.ps1
@@ -0,0 +1,11 @@
+﻿# Get all mounted volumes
+$volumes = Get-Volume | Where-Object { $_.DriveLetter -ne $null }
+
+# Loop through each volume and check BitLocker status
+foreach ($volume in $volumes) {
+    $driveLetter = $volume.DriveLetter + ":"
+    Write-Host "Checking BitLocker status for volume: $driveLetter"
+    
+    # Execute manage-bde command to check BitLocker status
+    manage-bde -status $driveLetter
+}
